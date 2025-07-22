a85eef20f348e8cbdf82d98e481a9321be2b8d8b
SherazAli-67 <soomrosheraz054@gmail.com>
2025-07-22 12:56:18 +0500

    Project init

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:text_summarizer_app/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Text Summarizer Bot',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomePage()
+    );
+  }
+}
+
